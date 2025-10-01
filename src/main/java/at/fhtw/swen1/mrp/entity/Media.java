965bfe978136999d89494544c53414c157421337
DeeQay <david.kedra.1@gmail.com>
2025-10-01 19:45:00 +0200

    create user and media entities

---
+++ src/main/java/at/fhtw/swen1/mrp/entity/Media.java
@@ -0,0 +1,37 @@
+package at.fhtw.swen1.mrp.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Media Entity für Database Representation
+ * Zwischenabgabe - grundlegende Media Data
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Media {
+    private Long id;
+    private String title;
+    private String description;
+    private String mediaType; // movie, series, book, etc.
+    private Integer releaseYear;
+    private List<String> genres;
+    private String ageRestriction;
+    private LocalDateTime createdAt;
+
+    public Media(String title, String description, String mediaType, Integer releaseYear) {
+        this.title = title;
+        this.description = description;
+        this.mediaType = mediaType;
+        this.releaseYear = releaseYear;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    // TODO: Validierungs-Methoden hinzufügen
+    // TODO: Rating-Berechnung hinzufügen
+}
---
+++ src/main/java/at/fhtw/swen1/mrp/entity/User.java
@@ -0,0 +1,32 @@
+package at.fhtw.swen1.mrp.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * User Entity für Database Representation
+ * Zwischenabgabe - grundlegende User Data
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Long id;
+    private String username;
+    private String passwordHash; // TODO: Niemals Klartext-Passwörter speichern
+    private String email;
+    private LocalDateTime createdAt;
+
+    public User(String username, String passwordHash, String email) {
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.email = email;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    // TODO: Validierungs-Methoden hinzufügen
+    // TODO: Password Verification Methode hinzufügen
+}
