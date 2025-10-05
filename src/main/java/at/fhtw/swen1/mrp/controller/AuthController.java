124e447da91291f10f094192fe9b6f5b1bc5c13b
DeeQay <david.kedra.1@gmail.com>
2025-10-05 10:15:00 +0200

    add controller stubs

---
+++ src/main/java/at/fhtw/swen1/mrp/controller/AuthController.java
@@ -0,0 +1,8 @@
+package at.fhtw.swen1.mrp.controller;
+
+import at.fhtw.swen1.mrp.service.AuthService;
+
+public class AuthController {
+    private AuthService authService;
+    // complete implementation
+}
---
+++ src/main/java/at/fhtw/swen1/mrp/controller/UserController.java
@@ -0,0 +1,8 @@
+package at.fhtw.swen1.mrp.controller;
+
+import at.fhtw.swen1.mrp.service.UserService;
+
+public class UserController {
+    private UserService userService;
+    // complete implementation
+}
